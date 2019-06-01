db8734ad046aefc79b500e8a28ef808588f3c4e9
Tanner Ryan <hello@tannerryan.ca>
2019-06-01 15:09:55 -0400

    Bump gist

--- gist.rb
+++ gist.rb
@@ -1,9 +1,9 @@
 class Gist < Formula
   desc "Unofficial command line tool for file uploads to GitHub gist"
   homepage "https://github.com/TheTannerRyan/gist"
-  url "https://github.com/TheTannerRyan/gist/releases/download/v1.0.2/gist-darwin_amd64.tar.gz"
-  version "1.0.2"
-  sha256 "2c60c777eee005c563b92a9c062daecef9a61456c19c25d5056ddf31c157d4ad"
+  url "https://github.com/TheTannerRyan/gist/releases/download/v2.0.0/gist-darwin_amd64.tar.gz"
+  version "2.0.0"
+  sha256 "3e0cc077de57d6c94e24125dd65b1542f66f58808832ae8fe2091172c2e2c13b"
 
   bottle :unneeded
 
